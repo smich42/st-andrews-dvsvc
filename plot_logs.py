6cdf4113e063521b6be932d30be5fd21464fc080
Stavros Michalovits <stmichalovits@gmail.com>
2024-04-20 22:27:31 +0100

    Plot itemised pages and flds

--- plot_logs.py
+++ plot_logs.py
@@ -10,6 +10,8 @@ itemised_page_pattern = re.compile(r"Itemised page: (.+)")
 
 total_responses = [0]
 mean_lscores = [0]
+itemised_pages = [0]
+itemised_flds = [0]
 itemisation_rates = [0]
 unique_fld_rate = [0]
 
@@ -25,6 +27,10 @@ with open("logs/dvsvc-spiders.log", "r") as file:
             total_responses.append(int(match_total_responses.group(1)))
 
             cur_responses = total_responses[-1] - total_responses[-2]
+
+            itemised_pages.append(itemised_pages[-1] + cur_itemised_count)
+            itemised_flds.append(itemised_flds[-1] + cur_unique_fld_count)
+
             itemisation_rates.append(cur_itemised_count / cur_responses)
             unique_fld_rate.append(cur_unique_fld_count / cur_responses)
 
@@ -45,31 +51,37 @@ with open("logs/dvsvc-spiders.log", "r") as file:
                 flds.add(fld)
 
 
-fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(12, 12))
+fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3, 2, figsize=(12, 8))
 
-ax1.plot(total_responses, mean_lscores, marker="o")
+ax1.plot(total_responses, itemised_flds)
 ax1.set_xlabel("Total Responses")
-ax1.set_ylabel("Discovered lscore")
-ax1.set_title("Discovered lscore over Total Responses")
+ax1.set_ylabel("Itemised FLDs")
+ax1.set_title("Itemised FLDs over Total Responses")
 ax1.grid(True)
 
-ax2.plot(total_responses, itemisation_rates, marker="o")
+ax2.plot(total_responses, itemised_pages, color="red")
 ax2.set_xlabel("Total Responses")
 ax2.set_ylabel("Itemised Pages")
 ax2.set_title("Itemised Pages over Total Responses")
 ax2.grid(True)
 
-ax3.plot(total_responses, itemisation_rates, marker="o")
+ax3.plot(total_responses, unique_fld_rate)
 ax3.set_xlabel("Total Responses")
-ax3.set_ylabel("Itemisation Rate")
-ax3.set_title("Itemisation Rate over Total Responses")
+ax3.set_ylabel("Discovery Rate")
+ax3.set_title("Discovery Rate over Total Responses")
 ax3.grid(True)
 
-ax4.plot(total_responses, unique_fld_rate, marker="o")
+ax4.plot(total_responses, itemisation_rates, color="red")
 ax4.set_xlabel("Total Responses")
-ax4.set_ylabel("Discovery Rate")
-ax4.set_title("Discovery Rate over Total Responses")
+ax4.set_ylabel("Itemisation Rate")
+ax4.set_title("Itemisation Rate over Total Responses")
 ax4.grid(True)
+
+ax5.plot(total_responses, mean_lscores, color="green")
+ax5.set_xlabel("Total Responses")
+ax5.set_ylabel("Discovered lscore")
+ax5.set_title("Discovered lscore over Total Responses")
+ax5.grid(True)
 
 # Adjust the spacing between subplots
 plt.tight_layout()
