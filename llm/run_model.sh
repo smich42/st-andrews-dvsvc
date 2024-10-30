8313686ca8c44af953eac4aae08787c498534ee7
Stavros Michalovits <stmichalovits@gmail.com>
2024-10-30 18:45:44 +0000

    Start ollama in foreground

--- llm/run_model.sh
+++ llm/run_model.sh
@@ -3,5 +3,13 @@
 MODEL_NAME=${1:-dvsvc-llm}
 
 ollama serve &
-ollama create ${MODEL_NAME} -f $(dirname "$0")/Modelfile
-ollama run $MODEL_NAME
+until /bin/curl -s http://localhost:11434/api/tags >/dev/null; do
+    sleep 2
+done
+
+if ! ollama list | grep -q "^${MODEL_NAME}"; then
+    ollama create ${MODEL_NAME} -f $(dirname "$0")/Modelfile
+fi
+
+pkill ollama
+exec ollama serve
